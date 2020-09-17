5f35fb53157a4cfd0c470968ad4b16e935946fa1
71075 <71075@MSI>
2020-09-17 14:52:16 +0800

    添加项目文件。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Flying_Cow_TMSMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
