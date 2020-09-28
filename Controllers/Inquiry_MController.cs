9f47bd4f8ea8688b3b6c9f9f03aba72859eab5a2
11371 <11371@LAPTOP-K99S8K9O>
2020-09-28 16:02:47 +0800

    赵博提交的第一个MVC

---
+++ Controllers/Inquiry_MController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flying_Cow_TMSMVC.Controllers
+{
+    public class Inquiry_MController : Controller
+    {
+        /// <summary>
+        /// 显示询价列表
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 审核页面
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult ShenHe(int Id)
+        {
+            ViewBag.Id = Id;
+            return View();
+        }
+        /// <summary>
+        /// 询价处理页面
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public IActionResult XJ_Deal(int Id)
+        {
+            ViewBag.Id = Id;
+            return View();
+        }
+        /// <summary>
+        /// 确认报价页面
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public IActionResult Confirm_quotation(int Id)
+        {
+            ViewBag.Id = Id;
+            return View();
+        }
+        
+    }
+}
---
+++ Controllers/Trace_MController1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flying_Cow_TMSMVC.Controllers
+{
+    public class Trace_MController1 : Controller
+    {
+        /// <summary>
+        /// 显示跟踪列表
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MapZg(int Id)
+        {
+            ViewBag.Id = Id;
+            return View();
+        }
+    }
+}
