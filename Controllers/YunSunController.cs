483a99f78689bba3ded9b3e599ed8082e6567709
86188 <86188@DESKTOP-E2QQERU>
2020-09-29 11:36:35 +0800

    1

---
+++ Controllers/YunSunController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flying_Cow_TMSMVC.Controllers
+{
+    public class YunSunController : Controller
+    {
+        //订单管理
+        public IActionResult Indexdjgl()
+        {
+
+            return View();
+        }
+        //处理订单
+        public IActionResult Indexcldj(string id,int eid)
+        {   
+            ViewBag.Id = id;
+            ViewBag.eid = eid;
+            return View();
+        }
+        //上传提货单
+        public IActionResult Add()
+        {
+            return View();
+        }
+        //上传提货单
+        public IActionResult dcthd(string id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+        //图片
+        //public IActionResult img()
+        //{
+        //    return View();
+        //}
+
+        //报价管理-待报价订单
+        public IActionResult dbjdd()
+        {
+            
+            return View();
+        }
+        //报价管理-订单报价
+        public IActionResult ddbj(string Id)
+        {
+            ViewBag.eid = Id;
+            return View();
+        }
+    }
+}
