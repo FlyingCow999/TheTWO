300ff3b703e42bebe90526ef1bf26fa3c715b321
Administrator <Administrator@DESKTOP-UNAQLM1>
2020-10-08 09:35:55 +0800

    爽的MVC

---
+++ Controllers/UserAddController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flying_Cow_TMSMVC.Controllers
+{
+    public class UserAddController : Controller
+    {
+        public IActionResult Login1()
+        {
+            return View();
+        }
+        //public IActionResult ShouIndex1(string name) //主页
+        //{
+        //    ViewBag.ss = name;
+        //    return View();
+        //}
+        public IActionResult ShouIndex(string name) //主页
+        {
+            ViewBag.ss = name;
+            return View();
+        }
+        public IActionResult enroll()
+        {
+            return View();
+        }
+    }
+}
