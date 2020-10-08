447b95371ff126396f95ab06711e5b59930342b7
11371 <11371@LAPTOP-K99S8K9O>
2020-10-08 11:38:07 +0800

    1

--- Controllers/Inquiry_MController.cs
+++ Controllers/Inquiry_MController.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Flying_Cow_TMSMVC.Controllers
 {
     public class Inquiry_MController : Controller
     {
+        private IConfiguration _configuration;
+        public Inquiry_MController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
         /// <summary>
         /// 显示询价列表
         /// </summary>
         /// <returns></returns>
         public IActionResult Index()
         {
+            string url = _configuration["apiUrl"];
+            ViewBag.url = url;
             return View();
         }
         /// <summary>
--- Controllers/Trace_MController1.cs
+++ Controllers/Trace_MController1.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Flying_Cow_TMSMVC.Controllers
 {
     public class Trace_MController1 : Controller
     {
+        private readonly IConfiguration _configuration;
+        public Trace_MController1(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
         /// <summary>
         /// 显示跟踪列表
         /// </summary>
         /// <returns></returns>
         public IActionResult Index()
         {
+            string url = _configuration["apiUrl"];
+            ViewBag.url = url;
             return View();
         }
         public IActionResult MapZg(int Id)
